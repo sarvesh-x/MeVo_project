735ef93b6825f6358cc85af701c049c523c35d1c
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-14 21:44:14 +0530

    Updated RootActivity.java

--- app/src/main/java/com/example/mevo/RootActivity.java
+++ app/src/main/java/com/example/mevo/RootActivity.java
@@ -1,10 +1,9 @@
 package com.example.mevo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,6 +16,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RootActivity extends AppCompatActivity {
 
+    FloatingActionButton mAddAlarmFab, mAddPersonFab;
+
+    ExtendedFloatingActionButton mAddFab;
+
+    TextView addAlarmActionText, addPersonActionText;
+
+    Boolean isAllFabsVisible;
 private ActivityRootBinding binding;
 
     @Override
@@ -33,6 +39,57 @@ private ActivityRootBinding binding;
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_root);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        mAddFab = findViewById(R.id.add_fab);
+        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+        mAddPersonFab = findViewById(R.id.add_person_fab);
+
+        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+        addPersonActionText = findViewById(R.id.add_person_action_text);
+
+        mAddAlarmFab.setVisibility(View.GONE);
+        mAddPersonFab.setVisibility(View.GONE);
+        addAlarmActionText.setVisibility(View.GONE);
+        addPersonActionText.setVisibility(View.GONE);
+        isAllFabsVisible = false;
+        mAddFab.shrink();
+        mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+                            mAddAlarmFab.show();
+                            mAddPersonFab.show();
+                            addAlarmActionText.setVisibility(View.VISIBLE);
+                            addPersonActionText.setVisibility(View.VISIBLE);
+                            mAddFab.extend();
+                            isAllFabsVisible = true;
+                        } else {
+                            mAddAlarmFab.hide();
+                            mAddPersonFab.hide();
+                            addAlarmActionText.setVisibility(View.GONE);
+                            addPersonActionText.setVisibility(View.GONE);
+                            mAddFab.shrink();
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+        mAddPersonFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(RootActivity.this, CreatePatientActivity.class);
+                        startActivity(intent);
+                    }
+                });
+        mAddAlarmFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(RootActivity.this, EmergencyActivity.class);
+                        startActivity(intent);
+                    }
+                });
 
     }
 
